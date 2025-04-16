aff6a40fc91ddab84dda1d289604054ae87e4220
jonata <jonatanery2016@gmail.com>
2025-04-15 21:06:27 -0400

    primeiro commit

---
+++ corretor redacao api/src/main/java/br/com/corretor_redacao_api/CorretorRedacaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.corretor_redacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorretorRedacaoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorretorRedacaoApiApplication.class, args);
+    }
+
+}
---
+++ corretor redacao api/src/test/java/br/com/corretor_redacao_api/CorretorRedacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.corretor_redacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorretorRedacaoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
