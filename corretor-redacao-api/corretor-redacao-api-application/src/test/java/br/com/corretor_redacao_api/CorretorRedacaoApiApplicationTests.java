a9e73bbf44d2dacbb9194b8a211d4de8376d289f
jonata <jonatanery2016@gmail.com>
2025-07-14 20:27:44 -0400

    feature/integracao-com-back-end conteinerizacao do back end e front end e container de banco de dados

---
+++ corretor-redacao-api/corretor-redacao-api-application/src/test/java/br/com/corretor_redacao_api/CorretorRedacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.corretor_redacao_api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorretorRedacaoApiApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
